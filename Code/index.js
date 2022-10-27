c3bd948e1445fc44d31a99f526589c1adacfa639
Xenko10 <101007014+Xenko10@users.noreply.github.com>
2022-10-27 10:46:46 +0200

    zajecia

---
+++ Code/index.js
@@ -0,0 +1,55 @@
+function Powitanie() {
+  let x;
+  x = prompt("Podaj imie", "");
+  alert("Witaj na stronie " + x + " 🔥🔥🔥");
+}
+function Dodaj() {
+  form1.w1.value = parseFloat(form1.a1.value) + parseFloat(form1.b2.value);
+}
+
+function Odejmij() {
+  form1.w1.value = parseFloat(form1.a1.value) - parseFloat(form1.b2.value);
+}
+
+function Mnoz() {
+  form1.w1.value = parseFloat(form1.a1.value) * parseFloat(form1.b2.value);
+}
+
+function Dziel() {
+  if (parseFloat(form1.b2.value) === 0) {
+    alert("Nie dziel przez 0!");
+  } else {
+    form1.w1.value = parseFloat(form1.a1.value) / parseFloat(form1.b2.value);
+  }
+}
+
+function Pierwiastek() {
+  let x = prompt("Podaj wartość do pierwiastka", "");
+  alert("Wartość pierwiastka z liczby " + x + " wynosi: " + Math.sqrt(x));
+}
+
+// function Silnia() {
+//   let x = prompt("Podaj wartość silni", "");
+//   for (i = 1; i < x; i++) {
+//     x = x * i;
+//   }
+//   alert("Wartość silni z liczby "x);
+// }
+
+// function Potega() {
+//   let a = prompt("Podaj wartość pierwszej liczby", "");
+//   let b = prompt("Podaj wartość drugiej liczby", "");
+//   for (i = 1; i < b; i++) {
+//     potega += a * i;
+//   }
+//   alert("Potega wynosi: " + potega);
+// }
+
+function Zmiana() {
+  div1.style.left = form2.a2.value + "px";
+  div1.style.top = form2.b2.value + "px";
+  div1.style.borderColor = form2.col.value;
+  div1.style.borderStyle = form2.style.value;
+  div1.style.borderWidth = form2.width.value + "px";
+  //   borderStyle borderWidth
+}
